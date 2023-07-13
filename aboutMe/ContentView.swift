8edb3d5049119894d8ff4f6a422fa034a9380f34
stacey <staceylee1229@gmail.com>
2023-07-13 14:04:45 -0700

    GitHub practice

--- aboutMe/ContentView.swift
+++ aboutMe/ContentView.swift
@@ -20,6 +20,7 @@ struct ContentView: View {
                 .cornerRadius(15)
                 .shadow(radius: 15)
                 .padding()
+        //practicing github
         }
     }
 struct ContentView_Previews: PreviewProvider {
